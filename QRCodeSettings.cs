80831c816c4482eaed1a309e54d9b7a5f6f2e930
Mustafa Pat <mustafapat@gmail.com>
2026-01-25 19:48:31 +0300

    Add files via upload

--- QRCodeSettings.cs
+++ QRCodeSettings.cs
@@ -2,53 +2,15 @@ using Nop.Core.Configuration;
 
 namespace Nop.Plugin.Widgets.QRCode;
 
-/// <summary>
-/// Represents QR Code plugin settings
-/// </summary>
 public class QRCodeSettings : ISettings
 {
-    /// <summary>
-    /// Gets or sets a value indicating whether the widget is enabled
-    /// </summary>
     public bool Enabled { get; set; }
-
-    /// <summary>
-    /// Gets or sets the QR code size in pixels
-    /// </summary>
     public int Size { get; set; }
-
-    /// <summary>
-    /// Gets or sets the widget zone where QR code should be displayed
-    /// </summary>
-    public string WidgetZone { get; set; }
-
-    /// <summary>
-    /// Gets or sets the hint text displayed below QR code
-    /// </summary>
-    public string HintText { get; set; }
-
-    /// <summary>
-    /// Gets or sets a value indicating whether to show border
-    /// </summary>
+    public string WidgetZone { get; set; } = string.Empty;
+    public string HintText { get; set; } = string.Empty;
     public bool ShowBorder { get; set; }
-
-    /// <summary>
-    /// Gets or sets the border color (hex)
-    /// </summary>
-    public string BorderColor { get; set; }
-
-    /// <summary>
-    /// Gets or sets the border width in pixels
-    /// </summary>
+    public string BorderColor { get; set; } = string.Empty;
     public int BorderWidth { get; set; }
-
-    /// <summary>
-    /// Gets or sets the border radius in pixels
-    /// </summary>
     public int BorderRadius { get; set; }
-
-    /// <summary>
-    /// Gets or sets the background color (hex)
-    /// </summary>
-    public string BackgroundColor { get; set; }
+    public string BackgroundColor { get; set; } = string.Empty;
 }
